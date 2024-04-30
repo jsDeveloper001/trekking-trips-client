d55d2eb211750149b5cff9fd56c227cebb9127ec
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-04-30 23:00:27 +0600

    responsive slider

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,35 @@
 import AllTouristsSpot from "../AllTouristsSpot/AllTouristsSpot";
 
 const Home = () => {
-    
+
     return (
         <div>
-            <AllTouristsSpot/>
-            
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full md:h-56 lg:h-screen">
+                    <img src={"https://i.ibb.co/Mpx9NP3/unnamed.jpg"} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full md:h-56 lg:h-screen">
+                    <img src={'https://i.ibb.co/LZ0tR4T/2022-12-31.jpg'} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full md:h-56 lg:h-screen">
+                    <img src={'https://i.ibb.co/wsM8bDb/2023-08-12.jpg'} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+
+            <AllTouristsSpot />
+
         </div>
     );
 };
